270b6d588c278d88a1a217c26de092ace510c1a3
mshherbakov <sherbakov.maks@gmail.com>
2020-03-01 17:09:20 +0500

    [WIP] add java modules

--- apiservice/src/main/java/module-info.java
+++ apiservice/src/main/java/module-info.java
@@ -3,11 +3,26 @@
  * @since 22.02.2020
  */
 module api.service.apiservice {
-    requires spring.boot.autoconfigure;
     requires spring.boot;
-    requires lombok;
-    requires jackson.annotations;
+    requires spring.boot.autoconfigure;
     requires spring.web;
-    requires reactor.core;
+    requires spring.beans;
+    requires spring.context;
+    requires spring.core;
+    requires spring.kafka;
+    requires kafka.clients;
     requires api.service.expr;
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.classmate; // need for Spring's ValidationAutoConfiguration
+//    requires lombok; // not working https://github.com/rzwitserloot/lombok/issues/2125
+
+    exports ru.sherb.microcalc.apiservice to spring.boot.devtools, spring.beans, spring.context;
+    exports ru.sherb.microcalc.apiservice.kafka to spring.beans, spring.context;
+    exports ru.sherb.microcalc.apiservice.service to spring.beans, spring.context;
+    exports ru.sherb.microcalc.apiservice.endpoint.v1 to spring.beans, spring.context;
+
+    opens ru.sherb.microcalc.apiservice to spring.core;
+    opens ru.sherb.microcalc.apiservice.kafka to spring.core;
 }
